b95af674faf6768a3e343693dc0157b38c32356b
Rohith Ramesh <rohithrameshrs44@gmail.com>
2022-10-21 12:49:37 +0530

    first style added

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,33 +1,64 @@
 import React from "react";
 import home1 from "../img/home1.png";
+import styled from "styled-components";
 
 const AboutSection = () => {
   return (
-    <div>
-      <div className="description">
+    <About>
+      <Description>
         <div className="title">
-          <div className="hide">
+          <Hide>
             <h2>We work to make</h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>
               Your <span>dreams</span> come
             </h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>true.</h2>
-          </div>
+          </Hide>
           <p>
             Contact for any photograpy or videography ideas that you have
             professionals with amaze skills
           </p>
-          <div className="image">
-            <img src={home1} alt="Profile pic" />
-          </div>
+          <button>More</button>
         </div>
-      </div>
-    </div>
+      </Description>
+      <Image>
+        <img src={home1} alt="Profile pic" />
+      </Image>
+    </About>
   );
 };
+
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+const Description = styled.div`
+  display: flex;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 70vh;
+    object-fit: cover;
+  }
+`;
+const Hide = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutSection;
---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,61 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+*{
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+}
+html{
+    box-sizing: inherit;
+    font-size: 62.5%;
+    font-weight: 400;
+}
+body{
+    background-color:#1b1b1b;
+
+}
+button{
+    font-size: 2rem;
+    font-weight: 600;
+    color: #fff;
+    padding: 1.2rem 3rem;
+    cursor: pointer;
+    border:3px solid #23d997;
+    background: transparent;
+    transition: all .1s ease-in-out;
+    &:hover{
+        transform: translateY(-2px);
+        box-shadow: 0 5px 10px rgba(0,0,0,0.5);
+    }
+    &:active,&:link{
+        transform: translateY(0px);
+        box-shadow: 0 2px 10px rgba(0,0,0,0.1);
+    }
+}
+    h2{
+        font-weight: lighter;
+        font-size: 4rem;
+
+    }
+    h3{
+        color:#fff;
+    }
+    p{
+        padding: 3rem 0rem;
+        color: #ccc;
+        font-size: 1.4rem
+    }
+    a{
+        font-size: 1.8rem;
+    }
+    span{
+        font-weight: bold;
+        color: #23d997;
+    }
+
+
+
+`;
+
+export default GlobalStyle;
